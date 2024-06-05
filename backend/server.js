e1a5eecdd94fed99058d2fb4738f2f3ede9c9a9f
Asish-Tammana <tammanaasish@gmail.com>
2024-06-05 12:54:20 +0530

    nodemon installed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express()
+dotenv.config()
 
 
 app.get("/", (req, res) => {
     res.send("hey")
 })
+
+const PORT  = process.env.PORT || 5000
 
 app.listen(5000, console.log("port running"))
