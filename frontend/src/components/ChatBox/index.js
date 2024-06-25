37f6cf42be74349a4736814f7372967e2f5ae317
Asish-Tammana <tammanaasish@gmail.com>
2024-06-25 14:41:06 +0530

    minor changes

--- frontend/src/components/ChatBox/index.js
+++ frontend/src/components/ChatBox/index.js
@@ -13,11 +13,11 @@ const ChatBox = ({ chatId }) => {
 
   const chatMessages = useSelector(state => state.chatMessages)
   const { messagesList } = chatMessages;
-  
+
 
   const dispatch = useDispatch()
 
-  const sendMessageToReceiver = async(e) => {
+  const sendMessageToReceiver = async (e) => {
     e.preventDefault();
 
     const newMsg = await dispatch(sendNewMessage(userMessage, chatId))
@@ -37,18 +37,25 @@ const ChatBox = ({ chatId }) => {
         <Typography>Chat Box</Typography>
         <UpdateGroupModal />
       </Box>
-      <Box sx={{ height: '90%', backgroundColor: 'pink', display: 'flex', flexDirection: 'column-reverse' }}>
+      <Box sx={{ height: '90%', backgroundColor: 'pink', display: 'flex', flexDirection: 'column-reverse', justifyContent: 'flex-end' }}>
         <form onSubmit={sendMessageToReceiver} style={{ display: 'flex', justifyContent: 'space-between', width: '100%', height: '7%' }}>
           <input type="text" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} />
           <button type="submit">Send</button>
         </form>
 
-        <Box sx={{ height: '93%', display: 'flex', flexDirection: 'column', overflowY: 'scroll', scrollbarWidth: 'none' }}>
+        <Box sx={{ height: '93%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', overflowY: 'scroll', scrollbarWidth: 'none' }}>
           {messagesList?.map(message => {
             const direction = message.sender._id === userInfo._id ? 'right' : 'left';
-            return (
-              <Typography key={message._id} style={{ textAlign: direction }}>{message.content}</Typography>
-            )
+            if (direction === 'right') {
+              return <Typography key={message._id} sx={{m:1}} style={{ textAlign: direction }}>{message.content}</Typography>
+            } else {
+              return (
+                <Box sx={{display: 'flex', m:1}}>
+                  {direction === 'left' && <img src={message.sender.picture} alt={message.sender.name} style={{ height: '30px', width: '30px' }} />}
+                  <Typography key={message._id} style={{ textAlign: direction }}>{message.content}</Typography>
+                </Box>
+              )
+            }
           })}
         </Box>
       </Box>
