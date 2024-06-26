372a66bb76ce5b7019ce396bc66216be787aa0c1
Asish-Tammana <tammanaasish@gmail.com>
2024-06-26 11:31:45 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,8 @@ io.on('connection', (socket) => {
         if(!chat.users) return console.log("no users available")
         
         chat.users.forEach(user => {
+            if (user._id == newMessageReceived.sender._id) return;
+            
             socket.in(user._id).emit("message received", newMessageReceived)
         })
 
