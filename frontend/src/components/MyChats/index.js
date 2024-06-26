e3901c2558b13aee2eb57d7f41a11ff8e3e4231e
Asish-Tammana <tammanaasish@gmail.com>
2024-06-26 21:38:49 +0530

    chats list updated

--- frontend/src/components/MyChats/index.js
+++ frontend/src/components/MyChats/index.js
@@ -1,9 +1,28 @@
-import { Box, List, ListItem, ListItemText, Typography, ListItemButton } from '@mui/material'
+import { Box, List, ListItem, ListItemText, Typography, ListItemButton, Skeleton, Stack } from '@mui/material'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { getAllChats } from '../../actions/chatActions'
 import NewGroupModal from '../NewGroupModal'
+
+const SkeletonContainer = ({ containerHeight }) => {
+  const skeletonHeight = 60; // Height of one Skeleton component in pixels
+  const containerHeightInPx = parseInt(containerHeight, 10); // Convert containerHeight to a number
+
+  const numberOfSkeletons = Math.ceil(containerHeightInPx / skeletonHeight);
+
+  const skeletons = Array.from({ length: numberOfSkeletons });
+
+  return (
+    <div style={{ height: containerHeight }}>
+      {skeletons.map((_, index) => (
+        <Box className="flex justify-between mt-2" >
+          <Skeleton key={index} variant="rounded" width={300} height={60} />
+        </Box>
+      ))}
+    </div>
+  );
+};
 
 const MyChats = ({chatId}) => {
 
@@ -13,7 +32,7 @@ const MyChats = ({chatId}) => {
   const userChats = useSelector(state => state.userChats)
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
-  const { chats } = userChats
+  const { chats, loading } = userChats
 
   useEffect(() => {
     dispatch(getAllChats())
@@ -23,11 +42,16 @@ const MyChats = ({chatId}) => {
 
 
   return (
-    <Box sx={{ width: '30%' }}>
+    <Box className="w-1/4 p-2 shadow">
       <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
         <Typography>My Chats</Typography>
         <NewGroupModal />
       </Box>
+
+    {loading && <Stack>
+      <SkeletonContainer containerHeight="90vh" />
+      </Stack>}
+
       <List>
         {chats?.map(eachChat => {
 
